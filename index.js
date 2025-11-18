f407d28f8f9fe3f443440f8bb15d176f5e5556da
Mohammad Mustafa <m93355606@gmail.com>
2025-11-18 23:44:36 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+function changeOpacity(){
+  const a = document.getElementById("a");
+
+  a.style.opacity = "0.5";
+
+}
+
+setTimeout(
+
+    function (){
+  const b = document.getElementById("a");
+
+  b.style.opacity = "1"; 
+}, 10); 
+
+  
