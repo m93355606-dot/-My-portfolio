3a828a2cdee276b3be13f6a80d39f50e4e2c8047
Mohammad Mustafa <m93355606@gmail.com>
2025-11-19 15:38:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-function changeOpacity(event) {
-  event.preventDefault(); // stop navigation
-  const a = document.getElementById("a");
-  const url = a.href;
 
-  a.style.opacity = "0"; // fade out fully
-
-  setTimeout(function() {
-    window.location.href = url; // navigate after fade
-  }, 500); // matches CSS transition duration
-}
